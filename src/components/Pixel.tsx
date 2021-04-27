cbc0821fc1dedce59b723916fe297e989e184c26
Robiquet <tom-robiquet@hotmail.co.uk>
2021-04-27 20:43:22 +0100

    Fixed styled components warning

--- src/components/Pixel.tsx
+++ src/components/Pixel.tsx
@@ -1,33 +1,24 @@
 import styled from "styled-components";
 
-// interface StyledPixelProps {
-//   backgroundColour: string
-// }
+interface StyledPixelProps {
+  backgroundColour: string;
+}
 
-const StyledPixel = styled.div`
+const StyledPixel = styled.div.attrs<StyledPixelProps>((props) => ({
+  style: {
+    background: props.backgroundColour,
+  },
+}))<StyledPixelProps>`
   display: flex;
   align-items: center;
   justify-content: center;
-  background: ${(props: { backgroundColour: string }) =>
-    props.backgroundColour};
   width: 100px;
   height: 100px;
   flex-shrink: 0;
   border: white 1px solid;
-  /* background-color: red;
-  animation-name: example;
-  animation-duration: 4s;
-
-  @keyframes example {
-  from {background-color: red;}
-  to {background-color: ${(props: { backgroundColour: string }) =>
-    props.backgroundColour}}
-} */
 `;
 
-const Pixel = (props: {colour:string}) => {
-
-
+const Pixel = (props: { colour: string }) => {
   return <StyledPixel backgroundColour={props.colour}></StyledPixel>;
 };
 
