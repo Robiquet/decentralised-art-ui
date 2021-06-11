e3bc482217fb519db0d0af26ee1bc33d07e86a33
Robiquet <tom-robiquet@hotmail.co.uk>
2021-06-11 19:46:36 +0100

    Added font awesome icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import React from "react";
 import "./App.scss";
 import Grid from "./components/Grid";
+import { StyleSheetManager } from "styled-components";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faHandPaper,
+  faPen
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faHandPaper, faPen);
 
 function App() {
   return (
-    <>
+    <StyleSheetManager disableVendorPrefixes>
       <Grid></Grid>
-    </>
+    </StyleSheetManager>
   );
 }
 
