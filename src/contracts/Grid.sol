aae8d99a327d7fe5b9610b3d6d624665536cf09e
Robiquet <tom-robiquet@hotmail.co.uk>
2021-06-22 21:42:27 +0100

    added colour storage

--- src/contracts/Grid.sol
+++ src/contracts/Grid.sol
@@ -1,11 +1,28 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Grid {
+    string[] public colours = [''];
     uint256 public rows;
     uint256 public cols;
 
     constructor(uint256 _rows, uint256 _cols) {
         rows = _rows;
         cols = _cols;
+        for (uint i = 1; i < _rows * _cols; i++) {
+            colours.push('');
+        }
+        
+    }
+    
+    function getColour(uint index) public view returns (string memory) {
+        return colours[index];
+    }
+    
+    function setColour(uint index, string memory colour) public {
+        colours[index] = colour;
+    }
+    
+    function getColours() public view returns (string[] memory) {
+        return colours;
     }
 }
